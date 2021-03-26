48413240278a29d0493e2f3d26b80d652ce7d2a6
ljagodz <ljagodz@gmail.com>
2021-03-25 23:21:13 -0400

    feature extraction module initial commit

---
+++ feature_extraction.py
@@ -0,0 +1,44 @@
+import os
+import torch
+from torchvision import datasets, models, transforms
+from torch.utils.data import DataLoader, Dataset
+import rcnn_data
+
+
+def main():
+
+    DATA_PATH = './data/dataset_by_run/dataset_by_run/'
+    FEATURE_PATH = DATA_PATH + 'features'
+
+    # construct vgg model.
+    vgg = models.vgg16(pretrained=True)
+
+    # transform to apply to every image.
+    data_transform = transforms.Compose([transforms.CenterCrop(154), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    # initialize datasets, put into list as they are all distinct sequences.
+    dataset_list = []
+    for i in range(9):
+        path_string = str(i) + '/'
+        dataset_list.append(rcnn_data.ImageSequenceFolder(os.path.join(DATA_PATH, path_string), transform=data_transform))
+
+    # may need to add mappings here?
+
+    if not os.path.exists(FEATURE_PATH):
+        os.mkdir(FEATURE_PATH)
+
+    for i in range(9):
+        file_number = 0  # all features assigned an arbitrary number for naming purposes.
+        data_loader = DataLoader(dataset_list[i], batch_size=1, shuffle=False)
+        for j, (rgb_image, img_image, label) in enumerate(data_loader):
+            rgb_feature = vgg.features(rgb_image)
+            img_feature = vgg.features(img_image)
+
+            rgb_feature_tensor = torch.from_numpy(rgb_feature.detach().numpy())
+            img_feature_tensor = torch.from_numpy(img_feature.detach().numpy())
+
+            rgb_img_combined_tensor = torch.cat((rgb_feature_tensor, img_feature_tensor), dim=1)
+
+
+if __name__ == "__main__":
+    main()
